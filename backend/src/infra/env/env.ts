fa2c6a32a809527dc88447a1dccb60eb723a7952
Felipe Santiago Morais <fillikz@gmail.com>
2025-12-02 02:56:39 -0300

    feat(EnvModule): configure environment variables with zod validation

---
+++ backend/src/infra/env/env.ts
@@ -0,0 +1,10 @@
+import z from "zod";
+
+export const envSchema = z.object({
+  DATABASE_URL: z.url(),
+  PORT: z.coerce.number().optional().default(3333),
+  JWT_PRIVATE_KEY: z.coerce.string(),
+  JWT_PUBLIC_KEY: z.coerce.string(),
+});
+
+export type Env = z.infer<typeof envSchema>;
