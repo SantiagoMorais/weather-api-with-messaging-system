bdccb7d98c9f4151daa27d023ccde8bb250b5e5e
Felipe Santiago Morais <fillikz@gmail.com>
2025-12-02 16:50:38 -0300

    test(FindManyWeatherLogRecent): tested the response of the use case

---
+++ backend/src/domain/weatherLog/application/use-cases/find-many-weather-log-recent.test.ts
@@ -0,0 +1,31 @@
+import { InMemoryWeatherLogRepository } from "test/repositories/in-memory-weather-log-repository";
+import { FindManyWeatherLogRecent } from "./find-many-weather-log-recent.usecase";
+import { makeWeatherLog } from "test/factories/make-weather-log";
+import { UniqueEntityId } from "src/core/entities/unique-entity-id";
+
+let inMemoryWeatherLogRepository: InMemoryWeatherLogRepository;
+let sut: FindManyWeatherLogRecent;
+
+describe("FindManyWeatherLogRecent use case", () => {
+  beforeEach(() => {
+    inMemoryWeatherLogRepository = new InMemoryWeatherLogRepository();
+    sut = new FindManyWeatherLogRecent(inMemoryWeatherLogRepository);
+  });
+
+  it("should be able to return the recent weather log", async () => {
+    for (let i = 0; i < 10; i++) {
+      const weatherLog = makeWeatherLog({}, new UniqueEntityId(i.toString()));
+      inMemoryWeatherLogRepository.weatherLogs.push(weatherLog);
+    }
+
+    const result = await sut.execute({ count: 5 });
+
+    expect(result.isSuccess()).toBe(true);
+    expect(result.value?.weatherLogs).toHaveLength(5);
+
+    const resultThatExceeds = await sut.execute({ count: 20 });
+
+    expect(resultThatExceeds.isSuccess()).toBe(true);
+    expect(resultThatExceeds.value?.weatherLogs).toHaveLength(10);
+  });
+});
