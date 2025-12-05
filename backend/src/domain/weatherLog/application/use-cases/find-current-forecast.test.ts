a4d3934e9bc7dcaa8f974a3b5e97e5ecaf322bc9
Felipe Santiago Morais <fillikz@gmail.com>
2025-12-05 01:30:21 -0300

    feat(FindCurrentForecastUseCase): test use case to return the latest forecast

---
+++ backend/src/domain/weatherLog/application/use-cases/find-current-forecast.test.ts
@@ -0,0 +1,33 @@
+import { IObservationStats } from "src/core/interfaces/observation-stats";
+import { makeWeatherLog } from "test/factories/make-weather-log";
+import { InMemoryWeatherLogRepository } from "test/repositories/in-memory-weather-log-repository";
+import { FindCurrentForecastUseCase } from "./find-current-forecast.usecase";
+
+let inMemoryWeatherLogRepository: InMemoryWeatherLogRepository;
+let sut: FindCurrentForecastUseCase;
+
+describe("FindCurrentForecast use case", () => {
+  beforeEach(() => {
+    inMemoryWeatherLogRepository = new InMemoryWeatherLogRepository();
+    sut = new FindCurrentForecastUseCase(inMemoryWeatherLogRepository);
+  });
+
+  it("should be able to find the current forecast", async () => {
+    const firstForecast = makeWeatherLog({
+      location: {
+        latitude: 54.1234,
+        longitude: -15.6789,
+        timezone: "Europe/London",
+      },
+    });
+    await inMemoryWeatherLogRepository.save(firstForecast);
+
+    const result = await sut.execute();
+    expect(result.isSuccess()).toBe(true);
+
+    const { currentForecast } = result.value as {
+      currentForecast: IObservationStats[];
+    };
+    expect(currentForecast).toHaveLength(1);
+  });
+});
---
+++ backend/src/domain/weatherLog/application/use-cases/find-current-forecast.usecase.ts
@@ -0,0 +1,21 @@
+import { TFindCurrentForecastResponse } from "src/core/interfaces/use-cases/weather-log/find-current-forecast-use-case";
+import { failure, success } from "src/core/result";
+import { DataNotFoundError } from "src/core/errors/data-not-found-error";
+import { Injectable } from "@nestjs/common";
+import { WeatherLogRepository } from "../repositories/weather-log.repository";
+
+@Injectable()
+export class FindCurrentForecastUseCase {
+  constructor(private weatherLogRepository: WeatherLogRepository) {}
+
+  async execute(): Promise<TFindCurrentForecastResponse> {
+    const weatherLog = await this.weatherLogRepository.findMostRecentLog();
+
+    if (!weatherLog)
+      return failure(
+        new DataNotFoundError("Current forecast was not created yet.")
+      );
+
+    return success({ currentForecast: weatherLog.currentForecastStats });
+  }
+}
