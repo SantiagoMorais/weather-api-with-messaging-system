b63d0f2bc50b97e4e0e4ecb46208310df7d58c30
Felipe Santiago Morais <fillikz@gmail.com>
2025-12-06 20:37:55 -0300

    feat(rabbitmq): implement consumer to process weather queue messages

---
+++ worker/rabbitmq/consumer.go
@@ -0,0 +1,66 @@
+package rabbitmq
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"time"
+	"worker/config"
+	"worker/types"
+
+	"github.com/streadway/amqp"
+)
+
+func ConsumeWeatherQueue(cfg *config.Config, handleMessage func(types.FullWeatherPayload)) {
+    conn, err := amqp.Dial(cfg.RabbitMQURL)
+    if err != nil {
+        log.Fatalf("Failed to connect to RabbitMQ: %v", err)
+    }
+    defer conn.Close()
+
+    ch, err := conn.Channel()
+    if err != nil {
+        log.Fatalf("Failed to open channel: %v", err)
+    }
+    defer ch.Close()
+
+    msgs, err := ch.Consume(
+        cfg.QueueName,
+        "",
+        false, 
+        false,
+        false,
+        false,
+        nil,
+    )
+    if err != nil {
+        log.Fatalf("Failed to register consumer: %v", err)
+    }
+
+    forever := make(chan bool)
+
+    go func() {
+        for d := range msgs {
+            var raw types.PythonWeatherPayload
+            if err := json.Unmarshal(d.Body, &raw); err != nil {
+                log.Printf("Error unmarshalling message: %v", err)
+                d.Nack(false, false)
+                continue
+            }
+
+            converted := types.FullWeatherPayload{
+                CreatedAt:              time.Now().Format(time.RFC3339),
+                Location:               raw.Location,
+                HourlyObservationStats: raw.Current,
+                CurrentForecastStats:   raw.Forecast24h,
+            }
+
+
+            handleMessage(converted)
+            d.Ack(false)
+        }
+    }()
+
+    fmt.Println("Worker running. Waiting for messages...")
+    <-forever
+}
