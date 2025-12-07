26e66ae9a9dcf9ad2cf4aab05878d30c533a8ab2
Felipe Santiago Morais <fillikz@gmail.com>
2025-12-07 05:46:39 -0300

    feat(Login): created animations for login card

--- frontend/src/pages/_public/login/index.tsx
+++ frontend/src/pages/_public/login/index.tsx
@@ -1,8 +1,9 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { SignInCardContent } from "./-components/sign-in-card-content";
+import { SignUpCardContent } from "./-components/sign-up-card-content";
 import { Card } from "@/components/ui/card";
 import { useState } from "react";
-import { SignUpCardContent } from "./-components/sign-up-card-content";
+import { motion, AnimatePresence, type Variants } from "framer-motion";
 
 export const Route = createFileRoute("/_public/login/")({
   component: RouteComponent,
@@ -16,15 +17,56 @@ function RouteComponent() {
   const changeAuthPage = () =>
     setPageSelected((prev) => (prev === "signin" ? "signup" : "signin"));
 
+  const [direction, setDirection] = useState<1 | -1>(1);
+
+  const handleChangePage = () => {
+    setDirection(pageSelected === "signin" ? 1 : -1);
+    changeAuthPage();
+  };
+
+  const variants: Variants = {
+    enter: (direction: number) => ({
+      x: direction > 0 ? 300 : -300,
+      opacity: 0,
+    }),
+    center: {
+      x: 0,
+      opacity: 1,
+    },
+    exit: (direction: number) => ({
+      x: direction > 0 ? -300 : 300,
+      opacity: 0,
+    }),
+  };
+
   return (
     <main className="flex h-dvh w-full items-center justify-center p-4">
-      <Card className="w-full max-w-96">
-        {pageSelected === "signin" ? (
-          <SignInCardContent changeAuthPage={changeAuthPage} />
-        ) : (
-          <SignUpCardContent changeAuthPage={changeAuthPage} />
-        )}
-      </Card>
+      <motion.div
+        className="w-full max-w-96"
+        initial={{ opacity: 0, x: -10, y: 20, rotate: -10 }}
+        animate={{ opacity: 1, x: 0, y: 0, rotate: 0 }}
+        transition={{ duration: 0.5, delay: 1 }}
+      >
+        <Card className="w-full max-w-96 overflow-hidden duration-300">
+          <AnimatePresence mode="wait" initial={false} custom={direction}>
+            <motion.div
+              key={pageSelected}
+              custom={direction}
+              variants={variants}
+              initial="enter"
+              animate="center"
+              exit="exit"
+              transition={{ type: "tween", duration: 0.3 }}
+            >
+              {pageSelected === "signin" ? (
+                <SignInCardContent changeAuthPage={handleChangePage} />
+              ) : (
+                <SignUpCardContent changeAuthPage={handleChangePage} />
+              )}
+            </motion.div>
+          </AnimatePresence>
+        </Card>
+      </motion.div>
     </main>
   );
 }
